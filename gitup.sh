9f211830ea524792eb7f879543b8d5674965d3e7
Johannes Skjærbæk <johannes.skjaerbaek@gmail.com>
2018-06-16 19:39:31 +0200

    Commenting gitup.sh

--- gitup.sh
+++ gitup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Copy this script to bin, or add this dir to the PATH var to run it anywhere
+# A command like this should work: sudo cp ./gitup.sh /usr/local/bin/gitup
+# With that done, you can use the script from anywhere by simply typing: gitup
+
+# Bear in mind that the script is made with the assumption that git credentials are cached/stored,
+# and that the remote is already added under the name origin
+
 BRANCH=`git status | grep -o 'On branch \w*[^\s]' | cut -d" " -f3`
 
 echo "Committing to branch $BRANCH"
