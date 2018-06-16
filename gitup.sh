10112e6b1e20fe4a161c5663d168c553fa67252e
Johnhundred <Johnhundred@users.noreply.github.com>
2018-06-16 20:51:39 +0200

    Update gitup.sh

--- gitup.sh
+++ gitup.sh
@@ -3,8 +3,8 @@
 # A command like this should work: sudo cp ./gitup.sh /usr/local/bin/gitup
 # With that done, you can use the script from anywhere by simply typing: gitup
 
-# Bear in mind that the script is made with the assumption that git credentials are cached/stored,
-# and that the remote is already added under the name origin
+# Bear in mind that the script is made with the assumption that git credentials are cached/stored.
+# If not, you will have to enter them. Script also assumes that the remote is as origin
 
 BRANCH=`git status | grep -o 'On branch \w*[^\s]' | cut -d" " -f3`
 
